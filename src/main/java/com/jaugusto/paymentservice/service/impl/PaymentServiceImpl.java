7af682f18dc39f773312c475972bfdc255d0679a
jaugusto <dev@augustolustosa.com>
2022-10-04 04:22:28 -0300

    add producer config

--- src/main/java/com/jaugusto/paymentservice/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/jaugusto/paymentservice/service/impl/PaymentServiceImpl.java
@@ -2,14 +2,26 @@ package com.jaugusto.paymentservice.service.impl;
 
 import com.jaugusto.paymentservice.model.Payment;
 import com.jaugusto.paymentservice.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.io.Serializable;
+
 @Service
+@RequiredArgsConstructor
 @Log4j2
 public class PaymentServiceImpl implements PaymentService {
+
+    private final KafkaTemplate<String, Serializable> kafkaTemplate;
     @Override
+    @SneakyThrows
     public void sendPayment(Payment payment) {
-        log.info("PAYMENT SERVICE IMPL ::: Recebi o pagamento", payment);
+        log.info("payment received!", payment);
+        Thread.sleep(5000);
+        log.info("sending payment to \"payment-topic\" topic ...");
+        kafkaTemplate.send("payment-topic", payment);
     }
 }
